ce78e45a82a3f288b9a1c7138ab9aa8230e3162e
Channarong Teanaram <channarong.te@cdg.co.th>
2020-05-01 17:31:18 +0700

    refecture

--- NongTimmerSystemOneLine/B_SETUP.ino
+++ NongTimmerSystemOneLine/B_SETUP.ino
@@ -3,10 +3,13 @@ SoftDMD dmd(2,1);
 DMD_TextBox box(dmd, 0, 1, 64, 16); 
 // Set Box (dmd, x, y, Height, Width) 
 
-
+const int sensorStart = 2;
+const int sensorStop = 3; 
 
 void setup() {
   Serial.begin(9600);
+  pinMode(sensorStart, INPUT);
+  pinMode(sensorStop, INPUT);
   dmd.setBrightness(10); // Set brightness 0 - 255 
   dmd.selectFont(Arial_Black_16); // Font used
   dmd.begin();     // Start DMD 
--- NongTimmerSystemOneLine/C_LOOP.ino
+++ NongTimmerSystemOneLine/C_LOOP.ino
@@ -1,12 +1,3 @@
-const int buttonStart = 2;     // the number of the pushbutton pin
-const int buttonStop = 3; 
-
-void setup() {
-    Serial.begin(9600);
-    pinMode(buttonStart, INPUT);
-    pinMode(buttonStop, INPUT);
-}
-
 unsigned long startTime = 0;
 //char *result = "      \0";
 char *result = malloc(7);
@@ -16,13 +7,12 @@ bool stateCurrent = false;
 
 void loop() {
   
-  startState = digitalRead(buttonStart);
-  stopState = digitalRead(buttonStop);
+  startState = digitalRead(sensorStart);
+  stopState = digitalRead(sensorStop);
   
   if(stateCurrent == false && startState == 0){
     stateCurrent = true;
     startTime = millis();
-   
   }
   
   if(stateCurrent == true && stopState == 0){
