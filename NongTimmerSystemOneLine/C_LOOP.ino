48e794dd6af9e54f265e18bb5ca40aa517be5d7a
mrnong789 <mrnong789@gmail.com>
2020-05-01 17:16:52 +0700

    Update C_LOOP.ino
    add sensor start and stop

--- NongTimmerSystemOneLine/C_LOOP.ino
+++ NongTimmerSystemOneLine/C_LOOP.ino
@@ -1,19 +1,40 @@
-char *result = malloc(7);
-int startTime = 0;
-void loop() {
-  long elapsedTime  =   millis() - startTime;
-  long timeAll  = elapsedTime;
+const int buttonStart = 2;     // the number of the pushbutton pin
+const int buttonStop = 3; 
 
-  int digitSec = timeAll / 1000;
-  int digitMilisec = timeAll % 1000;
-  sprintf(result, "%02d.%03d ",digitSec ,digitMilisec);
-  Serial.println(digitSec);
-  if(digitSec >= 99){
-   
-   dmd.drawString(8,1,"00.000 ");
-   startTime = millis();
-   
-  }else{
-     dmd.drawString(8,1,result);
-  }
+void setup() {
+    Serial.begin(9600);
+    pinMode(buttonStart, INPUT);
+    pinMode(buttonStop, INPUT);
 }
+
+unsigned long startTime = 0;
+//char *result = "      \0";
+char *result = malloc(7);
+int startState = 1;
+int stopState = 1;
+bool stateCurrent = false;
+
+void loop() {
+  
+  startState = digitalRead(buttonStart);
+  stopState = digitalRead(buttonStop);
+  
+  if(stateCurrent == false && startState == 0){
+    stateCurrent = true;
+    startTime = millis();
+   
+  }
+  
+  if(stateCurrent == true && stopState == 0){
+    stateCurrent = false;
+  }
+  
+ if(stateCurrent == true) {
+  unsigned long elapsedTime =  millis() - startTime;
+  unsigned int digitSec = (elapsedTime / 1000);
+  unsigned int digitMilisec = elapsedTime % 1000;
+  sprintf(result, "%02d.%03d ",digitSec ,digitMilisec);
+  dmd.drawString(8,1,result);
+ }
+}
+
