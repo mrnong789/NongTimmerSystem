b3cf6327f35e7339cdb35d1aeb8ac0af44525170
mrnong789 <mrnong789@gmail.com>
2020-05-18 06:50:49 +0700

    add sensor start and finist

--- NongTimmerSystemOneLine/B_SETUP.ino
+++ NongTimmerSystemOneLine/B_SETUP.ino
@@ -10,8 +10,8 @@ void setup() {
   Serial.begin(9600);
   pinMode(sensorStart, INPUT);
   pinMode(sensorStop, INPUT);
-  dmd.setBrightness(10); // Set brightness 0 - 255 
+  dmd.setBrightness(255); // Set brightness 0 - 255 
   dmd.selectFont(Arial_Black_16); // Font used
   dmd.begin();     // Start DMD 
- // box.print("00.000"); // Display TEXT SFESS
+  dmd.drawString(8,1,"00.000");
 }
